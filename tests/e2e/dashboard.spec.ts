fc98f03f0dcd608e48a251ac88dc559d9a2cc54f
Gyan <gyanprakasht987@gmail.com>
2026-07-04 18:31:09 +0530

    /* eslint-disable @typescript-eslint/no-unused-vars */

--- tests/e2e/dashboard.spec.ts
+++ tests/e2e/dashboard.spec.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */ 
 import { test, expect } from '@playwright/test';
 
 test.describe('Dashboard User Flows', () => {
